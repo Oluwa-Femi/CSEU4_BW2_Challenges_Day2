2c23a1b478b743d3a1dfab047b26b4ca5901d64d
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-07-29 12:50:03 +0100

    Two sums py and js

--- two_sum/two_sum.py
+++ two_sum/two_sum.py
@@ -5,4 +5,9 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        for i in range(len(nums)):
+            mid = target - nums[i]
+            for j in range(i+1,len(nums)):
+                if nums[j] == mid:
+                    return [i,j]
         
